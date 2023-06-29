ffb42f615e76a72338cc8d8c4d5f8d1655c0d29b
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-06-29 12:25:58 +0200

    first commit

---
+++ resources/views/mails/new-contact-mail.blade.php
@@ -0,0 +1,8 @@
+{{-- <h1>Ciao Host!</h1>
+<p>
+    Hai ricevuto un nuovo messaggio, ecco qui i dettagli:<br>
+    Nome: {{ $lead->name }}<br>
+    Email: {{ $lead->address }}<br>
+    Messaggio:<br>
+    {{ $lead->message }}
+</p> --}}
