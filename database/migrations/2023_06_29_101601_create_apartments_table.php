dc5ebddc3ec46f0301d0a3d1f14b6e86f4d57d14
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-06-30 10:44:03 +0200

    clear comment

--- database/migrations/2023_06_29_101601_create_apartments_table.php
+++ database/migrations/2023_06_29_101601_create_apartments_table.php
@@ -41,25 +41,3 @@ return new class extends Migration {
         Schema::dropIfExists('apartments');
     }
 };
-
-// public function up()
-// {
-//     Schema::table('projects', function (Blueprint $table) {
-//         $table->unsignedBigInteger('type_id')->nullable();
-//         $table->foreign('type_id')->references('id')->on('types');
-
-//     });
-// }
-
-// /**
-//  * Reverse the migrations.
-//  *
-//  * @return void
-//  */
-// public function down()
-// {
-//     Schema::table('projects', function (Blueprint $table) {
-//         $table->dropForeign('projects_type_id_foreign');
-//         $table->dropColumn('type_id');
-//     });
-// }
