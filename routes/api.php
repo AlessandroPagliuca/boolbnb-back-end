e3d485df3d5c4b59fe4a5f023d85d502ee27261b
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-07-04 09:40:20 +0200

    Upgraded api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApartmentController;
+use App\Http\Controllers\Api\MessageController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/apartments', [ApartmentController::class, 'index']);
+Route::get('/apartment/{slug}', [ApartmentController::class, 'show']);
+Route::post('/messages', [MessageController::class, 'store']);
