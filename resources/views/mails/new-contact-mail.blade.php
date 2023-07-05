41ee7e7c8b5228ffc2e116015e6bfe167c333b84
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-07-05 12:35:31 +0200

    upgraded mail

--- resources/views/mails/new-contact-mail.blade.php
+++ resources/views/mails/new-contact-mail.blade.php
@@ -1,8 +1,14 @@
-{{-- <h1>Ciao Host!</h1>
-<p>
-    Hai ricevuto un nuovo messaggio, ecco qui i dettagli:<br>
-    Nome: {{ $lead->name }}<br>
-    Email: {{ $lead->address }}<br>
-    Messaggio:<br>
-    {{ $lead->message }}
-</p> --}}
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-flex justify-content-start p-3">
+        <h1>Hi host!</h1>
+        <p>
+            You have received a new message, here are the details:<br>
+            Name: {{ $message->name }}<br>
+            Email: {{ $message->email }}<br>
+            Message:<br>
+            {{ $message->message }}
+        </p>
+    </div>
+@endsection
