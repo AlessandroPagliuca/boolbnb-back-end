e82f072a43b766d3f7520ff316ed53d43e291ca9
anashamimi <anashamimi2@gmail.com>
2023-07-11 15:36:56 +0200

    fix responsive edit

--- resources/views/host/apartments/edit.blade.php
+++ resources/views/host/apartments/edit.blade.php
@@ -9,22 +9,23 @@
         <h1 class="py-4">Edit your apartment: {{ $apartment->title }}</h1>
 
 
-        <form action="{{ route('host.apartments.update', $apartment->slug) }}" onsubmit="validateForm(event)" id="editForm" method="POST" enctype="multipart/form-data" class="row">
+        <form action="{{ route('host.apartments.update', $apartment->slug) }}" onsubmit="validateForm(event)" id="editForm"
+            method="POST" enctype="multipart/form-data" class="row">
             @csrf
             @method('PUT')
-            <div  class="col-6 border-pink mb-3">
+            <div class="col-6 border-pink mb-3">
                 <label for="title" class="fw-bold">Title</label>
                 <input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
-                    id="title" value="{{ old('title', $apartment->title) }}"  minlength="3" maxlength="100" required>
+                    id="title" value="{{ old('title', $apartment->title) }}" minlength="3" maxlength="100" required>
                 @error('title')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
-            
+
             <div class="col-6 border-pink mb-3">
                 <label for="description" class="fw-bold">Description</label>
                 <input type="text" class="form-control @error('description') is-invalid @enderror" name="description"
-                    id="description" value="{{ old('description', $apartment->description) }}"  minlength="10" required>
+                    id="description" value="{{ old('description', $apartment->description) }}" minlength="10" required>
                 @error('description')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -33,8 +34,8 @@
                 <label for="main_img" class="form-label fw-bold">Insert main image</label>
                 <input class="form-control" type="file" id="main_img" name="main_img">
             </div>
-             
-            <div class="col-3 border-pink mb-3">
+
+            <div class="col-6 col-md-3 border-pink mb-3">
                 <label for="rooms" class="fw-bold">Rooms</label>
                 <input type="text" class="form-control @error('rooms') is-invalid @enderror" name="rooms"
                     id="rooms" value="{{ old('rooms', $apartment->rooms) }}" min="1" max="6" required>
@@ -42,7 +43,7 @@
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
-            <div class="col-3 border-pink mb-3">
+            <div class="col-6 col-md-3 border-pink mb-3">
                 <label for="beds" class="fw-bold">Beds</label>
                 <input type="text" class="form-control @error('beds') is-invalid @enderror" name="beds" id="beds"
                     value="{{ old('beds', $apartment->beds) }}" min="1" max="10" required>
@@ -51,18 +52,20 @@
                 @enderror
             </div>
 
-            <div class="col-3 border-pink mb-3">
+            <div class="col-6 col-md-3 border-pink mb-3">
                 <label for="bathrooms" class="fw-bold">Bathrooms</label>
                 <input type="text" class="form-control @error('bathrooms') is-invalid @enderror" name="bathrooms"
-                    id="bathrooms" value="{{ old('bathrooms', $apartment->bathrooms) }}" min="1" max="3" required>
+                    id="bathrooms" value="{{ old('bathrooms', $apartment->bathrooms) }}" min="1" max="3"
+                    required>
                 @error('bathrooms')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
-            <div class="col-3 border-pink mb-3">
-                <label for="square_meters" class="fw-bold">Square meters (m²)</label>
+            <div class="col-6 col-md-3 border-pink mb-3">
+                <label for="square_meters" class="fw-bold">Square meters</label>
                 <input type="text" class="form-control @error('square_meters') is-invalid @enderror" name="square_meters"
-                    id="square_meters" value="{{ old('square_meters', $apartment->square_meters) }}" min="0" max="500" required>
+                    id="square_meters" value="{{ old('square_meters', $apartment->square_meters) }}" min="0"
+                    max="500" required>
                 @error('square_meters')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -70,7 +73,8 @@
             <div class="col-9 border-pink mb-3">
                 <label for="address" class="fw-bold">Address</label>
                 <input type="text" class="form-control @error('address') is-invalid @enderror" name="address"
-                    id="address" value="{{ old('address', $apartment->address) }}" minlength="3" maxlength="255" required>
+                    id="address" value="{{ old('address', $apartment->address) }}" minlength="3" maxlength="255"
+                    required>
                 @error('address')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -78,17 +82,18 @@
             <div class="col-3 border-pink mb-3">
                 <label for="zipcode" class="fw-bold">zipcode</label>
                 <input type="text" class="form-control @error('zipcode') is-invalid @enderror" name="zipcode"
-                    id="zipcode" value="{{ old('zipcode', $apartment->zipcode) }}" minlength="1" maxlength="100" required>
+                    id="zipcode" value="{{ old('zipcode', $apartment->zipcode) }}" minlength="1" maxlength="100"
+                    required>
                 @error('zipcode')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
-            
+
             <!--City-->
             <div class="col-6 border-pink mb-3">
                 <label for="city" class="fw-bold">City</label>
-                <input type="text" class="form-control @error('city') is-invalid @enderror" name="city" id="city"
-                    value="{{ old('city', $apartment->city) }}" minlength="3" maxlength="100" required>
+                <input type="text" class="form-control @error('city') is-invalid @enderror" name="city"
+                    id="city" value="{{ old('city', $apartment->city) }}" minlength="3" maxlength="100" required>
                 @error('city')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -97,18 +102,20 @@
             <div class="col-6 border-pink mb-3">
                 <label for="country" class="fw-bold">Country</label>
                 <input type="text" class="form-control @error('country') is-invalid @enderror" name="country"
-                    id="country" value="{{ old('country', $apartment->country) }}" minlength="1" maxlength="100" required>
+                    id="country" value="{{ old('country', $apartment->country) }}" minlength="1" maxlength="100"
+                    required>
                 @error('country')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <!--country-->
-            
+
 
             <div class="col-6 border-pink mb-3">
                 <label for="price" class="fw-bold">Price per night</label>
                 <input type="text" class="form-control @error('price') is-invalid @enderror" name="price"
-                    id="price" value="{{ old('price', $apartment->price) }}" min="1" max="99999" required>
+                    id="price" value="{{ old('price', $apartment->price) }}" min="1" max="99999"
+                    required>
                 @error('price')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -127,20 +134,20 @@
                         <p class="text-dark">Select one or more services:</p>
                     </div>
                     @foreach ($services as $service)
-                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="col-12 col-sm-6 col-md-4">
 
-                        <input type="checkbox" name="services[]" value="{{ $service->id }}" class="form-check-input"
-                        {{ in_array($service->id, old('services', $apartment->services->pluck('id')->toArray())) ? 'checked' : '' }}>
-                        @if ($service->icon == 'instagram fa-rotate-180')
-                            <i class="fa-brands fa-{{ $service->icon }} px-2"></i>
-                        @else
-                            <i class="fa-solid fa-{{ $service->icon }} px-2"></i>
-                        @endif
-                        <label for="services[]" class="form-check-label">{{ $service->name }}</label>
-                    </div>
-                
-                @endforeach
-                <div class="fs-3 text-danger" id="servicesError" ></div>
+                            <input type="checkbox" name="services[]" value="{{ $service->id }}"
+                                class="form-check-input"
+                                {{ in_array($service->id, old('services', $apartment->services->pluck('id')->toArray())) ? 'checked' : '' }}>
+                            @if ($service->icon == 'instagram fa-rotate-180')
+                                <i class="fa-brands fa-{{ $service->icon }} px-2"></i>
+                            @else
+                                <i class="fa-solid fa-{{ $service->icon }} px-2"></i>
+                            @endif
+                            <label for="services[]" class="form-check-label">{{ $service->name }}</label>
+                        </div>
+                    @endforeach
+                    <div class="fs-3 text-danger" id="servicesError"></div>
 
 
 
@@ -149,42 +156,40 @@
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
-            
-            
-           
-        <div class="container pt-3">
-        <!--btn save and reset-->
-            <button type="submit" class="btn btn-dark">Save</button>
-            <button type="reset" class="btn btn-primary text-white">Reset</button>
-            
-        </div>
+
+
+
+            <div class="container pt-3">
+                <!--btn save and reset-->
+                <button type="submit" class="btn btn-dark">Save</button>
+                <button type="reset" class="btn btn-primary text-white">Reset</button>
+
+            </div>
 
         </form>
     </div>
 @endsection
 
 <script>
+    function validateForm(event) {
+        event.preventDefault(); // Prevents default form submission behavior
 
-        function validateForm(event) {
-            event.preventDefault(); // Prevents default form submission behavior
+        var checkboxes = document.querySelectorAll('input[name="services[]"]');
+        var isChecked = false;
 
-            var checkboxes = document.querySelectorAll('input[name="services[]"]');
-            var isChecked = false;
-
-            for (var i = 0; i < checkboxes.length; i++) {
-                if (checkboxes[i].checked) {
-                    isChecked = true;
-                    break;
-                }
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].checked) {
+                isChecked = true;
+                break;
             }
-            var servicesError = document.getElementById('servicesError');
-            if (!isChecked) {
-                servicesError.textContent = 'Please select at least one service.';
-                return false;
-            }else{
-                document.getElementById('editForm').submit();
-            }
-
         }
-</script>
+        var servicesError = document.getElementById('servicesError');
+        if (!isChecked) {
+            servicesError.textContent = 'Please select at least one service.';
+            return false;
+        } else {
+            document.getElementById('editForm').submit();
+        }
 
+    }
+</script>
