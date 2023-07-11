a7fd183db8e78b0c47804750c9ede335d2e80793
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-07-11 20:02:24 +0200

    fix mail-contact-mailtrap

--- resources/views/mails/new-contact-mail.blade.php
+++ resources/views/mails/new-contact-mail.blade.php
@@ -1,13 +1,7 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="d-flex justify-content-start p-3">
-        <h2>Hi host!</h2>
-        <p>
-            You have received a new message, here are the details:<br>
-            Email: {{ $message->email }}<br>
-            Message:<br>
-            {{ $message->message }}
-        </p>
-    </div>
-@endsection
+<h2>Hi host!</h2>
+<p>
+    You have received a new message, here are the details:<br>
+    Email: {{ $lead->email }}<br>
+    Message:<br>
+    {{ $lead->message }}
+</p>
